ae9bdda77713408a1b99910822256dfd98b9e2f2
loureq177 <142215274+loureq177@users.noreply.github.com>
2026-06-14 23:13:21 +0200

    chore: reorder flatpaks and remove gitignore

--- flatpaks.sh
+++ flatpaks.sh
@@ -1,9 +1,9 @@
 export FLATPAK_APPS=(
     app.zen_browser.zen
+    org.chromium.Chromium
     com.spotify.Client
     com.visualstudio.code
     com.discordapp.Discord
     org.freecadweb.FreeCAD
     com.prusa3d.PrusaSlicer
-    org.chromium.Chromium
 )
